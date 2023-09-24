0a197b1e4cb9bd8e81dd5d40eacf1e264e8de383
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-09-24 17:41:52 +0100

    Made Hero responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components"
-import { colors, mixins, values } from "../../StyledComponents/Styles"
+import { colors, devices, mixins, values } from "../../StyledComponents/Styles"
 import PageButton from "../PageButton/PageButton"
 import HeroImage from "./HeroImage/HeroImage"
 import chain from '/images/chain.svg'
 import bulb from '/images/bulb.svg'
 import flare from '/images/flare.svg'
+import line from '/images/line.svg'
+import { Slide } from "react-awesome-reveal"
 
 const HeroDisplay = styled.main`
-    positon: relative;
+    position: relative;
     width: ${values.width};
     height: ${values.height};
     padding: 0 4em;
@@ -16,7 +18,39 @@ const HeroDisplay = styled.main`
     justify-content: center;
     border-bottom: 0.02em solid ${colors.borderColor};
     font-family: ${values.font};
-    margin: 0 0 3em 0;
+    margin: 3em 0 3em 0;
+
+    @media screen and ${devices.tablet} {
+        padding: 0 2em;
+        flex-direction: column;
+    }
+
+    #info{
+        position: absolute;
+        top: 5%;
+        right: 5%;
+        font-size: clamp(calc(${values.largeText} * 0.2), 5vw, calc(${values.largeText} * 0.7));
+
+        @media screen and ${devices.tablet} {
+            position: relative;
+            top: 7%;
+            left: 0;
+            width: 100%;
+            margin: auto;
+            text-align: center;
+        }
+
+        #line{
+            position: absolute;
+            width: 50%;
+            bottom: -40%;
+            right: 0;
+
+            @media screen and ${devices.tablet} {
+                right: 20%;
+            }
+        }
+    }
 
     aside{
         position: relative;
@@ -25,6 +59,15 @@ const HeroDisplay = styled.main`
         display: flex;
         flex-direction: column;
         gap: 1em;
+
+        @media screen and ${devices.tablet} {
+            text-align: center;
+
+            p{
+                text-align: center;
+                padding: 0 3em;
+            }
+        }
 
         #flare{
             position: absolute;
@@ -38,9 +81,18 @@ const HeroDisplay = styled.main`
             position: relative;
             width: 100%;
             line-height: 1em;
-            font-size: ${values.extraLargeText};
+            font-size: clamp(calc(${values.largeText} * 1), 5vw, ${values.extraLargeText});
             font-weight: 900;
             font-family: ${values.bgFont};
+
+            @media screen and ${devices.tablet} {
+                text-align: center;
+
+                #bulb, #chain{
+                    width: 5%;
+                }
+            }
+
             span{
                 color: ${colors.pink};
             }
@@ -49,6 +101,10 @@ const HeroDisplay = styled.main`
                 position: absolute;
                 top: -50%;
                 right: 15%;
+
+                @media screen and ${devices.tablet} {
+                    right: 25%;
+                }
             }
 
             #chain{
@@ -71,6 +127,10 @@ const HeroDisplay = styled.main`
         padding: .5em 2em;
         background: ${mixins.gradient};
 
+        @media screen and ${devices.tablet} {
+            margin: auto;
+        }
+
         &:hover, &.activeLink{
             color: ${colors.white};
         }
@@ -80,33 +140,37 @@ const HeroDisplay = styled.main`
 export default function Hero() {
   return (
     <HeroDisplay>
-
+        <h1 id="info">
+            Igniting a Revolution in HR Innovation
+            <img src={line} alt="line" id='line' />
+        </h1>
         <aside>
             <img src={flare} alt="flare" id="flare" />
+            <Slide direction="left" delay={1} cascade={true} triggerOnce={true}>
+                    <h1>
+                        getlinked Tech Hackathon 
+                        <span>1.0</span>
+                        <img src={chain} alt="chain" id="chain" />
+                        <img src={bulb} alt="bulb" id="bulb" />
+                    </h1>
+                    <p>
+                        Participate in getlinked tech Hackathon 2023 stand 
+                        a chance to win a Big prize
+                    </p>
 
-            <h1>
-                getlinked Tech Hackathon 
-                <span>1.0</span>
-                <img src={chain} alt="chain" id="chain" />
-                <img src={bulb} alt="bulb" id="bulb" />
-            </h1>
-            <p>
-                Participate in getlinked tech Hackathon 2023 stand 
-                a chance to win a Big prize
-            </p>
 
+                    <PageButton
+                        title={'Register'}
+                        link={'/signup'}
+                        className={'register'}
+                    />
 
-            <PageButton
-                title={'Register'}
-                link={'/signup'}
-                className={'register'}
-            />
-
-            <h2>
-                00<small>H</small> &nbsp;
-                00<small>M</small> &nbsp;
-                00<small>S</small>
-            </h2>
+                    <h2>
+                        00<small>H</small> &nbsp;
+                        00<small>M</small> &nbsp;
+                        00<small>S</small>
+                    </h2>
+            </Slide>
         </aside>
 
         <HeroImage />
--- src/components/Hero/HeroImage/HeroImage.jsx
+++ src/components/Hero/HeroImage/HeroImage.jsx
@@ -3,28 +3,21 @@ import orb from '/images/orb.svg'
 import matrix from '/images/matrix.svg'
 import flare from '/images/flare.png'
 import styled from 'styled-components'
-import { values } from '../../../StyledComponents/Styles'
+import { values, devices } from '../../../StyledComponents/Styles'
 
 const MainImage = styled.section`
     position: relative;
     width: calc(${values.width} * 1);
     height: 100%;
-    heigth: 100%; 
+    height: 100%; 
     bottom: -5.3%;
     right: 0;
     margin:  2em 0 0 0;
 
-    h1{
-        font-size: ${values.largeText};
-
-        #line{
-            positon: absolute;
-            width: 30%;
-            bottom: 0;
-            right: 0;
-        }
+    @media screen and ${devices.tablet} {
+        bottom: 0;
     }
-    
+        
     img{
         position: absolute;
         width: 100%;
@@ -37,13 +30,13 @@ const MainImage = styled.section`
 
 export default function HeroImage() {
     return(
-        <MainImage>
+        <MainImage className='animate__animated animate__zoomIn'>
             <span>
                 <img src={matrix} alt="matrix" id='matrix' />
                 <img src={man} alt="person" id='man' />
                 <img src={flare} alt="flare" id='flare' />
                 <img src={orb} alt="orb" id='orb' />
-            </span>          
+            </span>
         </MainImage>
     )
 }
